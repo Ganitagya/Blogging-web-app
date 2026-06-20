8299857b13433f617a1ee36c5f76d247eb9b3a07
Ganitagya <akash.mhptr@gmail.com>
2026-06-20 12:57:30 +0400

    File uploads and profile picture chnages

--- schemas.py
+++ schemas.py
@@ -1,13 +1,16 @@
-from pydantic import BaseModel, ConfigDict, Field, EmailStr
-
 from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
 
 class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50)
     email: EmailStr = Field(max_length=120)
 
+
 class UserCreate(UserBase):
     password: str = Field(min_length=8)
+
 
 class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -17,17 +20,20 @@ class UserPublic(BaseModel):
     image_file: str | None
     image_path: str
 
+
 class UserPrivate(UserPublic):
     email: EmailStr
+
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
-    image_file: str | None = Field(default=None, min_length=1, max_length=200)
+
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -37,9 +43,11 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
+
 class PostUpdate(BaseModel):
     title: str | None = Field(default=None, min_length=1, max_length=100)
     content: str | None = Field(default=None, min_length=1)
+
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
